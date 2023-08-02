5b3c2acb3c11a2f0191224faf3abddcd903bfe03
hithaishisurendra <114680442+hithaishisurendra@users.noreply.github.com>
2023-08-02 09:58:16 +0530

    Logger

---
+++ day 7/App.java
@@ -0,0 +1,10 @@
+package com.telstra;
+
+public class App {
+
+	public static void main(String[] args) {
+		EmployeeService es=new EmployeeService();
+		es.addEmployee();
+	}
+
+}
---
+++ day 7/EmployeeService.java
@@ -0,0 +1,20 @@
+package com.telstra;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EmployeeService {
+	
+	private static final Logger logger= LoggerFactory.getLogger(EmployeeService.class);
+	
+	
+	void addEmployee()
+	{
+		logger.error("ERROR");
+		logger.warn("WARNING");
+		logger.info("INFO");
+		logger.debug("DEBUG");
+		
+		
+	}
+}
